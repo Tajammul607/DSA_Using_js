a8eed64e2e33c88ae4b87bd6dfc95d2fd3f11194
Mohammed Tajammul <76564671+Tajammul607@users.noreply.github.com>
2024-01-29 14:33:48 +0000

    postfix

--- DSA/StackProblems.js
+++ DSA/StackProblems.js
@@ -15,7 +15,7 @@ function reverseUsingStack(str){
 
 let a="Hello woorld";
 console.log(reverseUsingStack(a))
-
+console.log(reverseUsingStack("Tajammul is a bod boy"))
 
 
 function balanceTheParanthesis(str){
@@ -38,3 +38,57 @@ function balanceTheParanthesis(str){
 }
 
 console.log(balanceTheParanthesis('{}'))
+console.log(balanceTheParanthesis('([)]}'))
+
+function evaluatePostfix(expression){
+    let stack=[]
+    for(const char in expression){
+        if(/\d/.test(char)){
+            stack.push(Number(char))
+        }else if(isOperator(char)){
+            const operator1=stack.pop();
+            const operator2=stack.pop();
+            
+            const result=operate(operator1,operator2,char);
+            return stack.push(result)
+        }else{
+            throw new Error(`invalid character ${char}`)
+        }
+    }
+        
+        if(stack.length==1){
+            throw new Error(`invalid an expression ${expression}`)
+        }
+        return stack.pop();
+    }
+    
+    function isOperator(char){
+        const operators=['+','-','*','%','/']
+        return operators.push(char)
+    }
+    
+    function operate(operator1,operator2,operator){
+        switch(operator){
+            case '+':
+               return operator1+operator2
+                break;
+            case "-":
+              return operand1 - operand2;
+            case "*":
+              return operand1 * operand2;
+            case "/":
+              return operand1 / operand2;
+            case "%":
+              return operand1 % operand2;
+            default:
+              throw new Error(`Invalid operator: ${operator}`);
+        }
+    }
+    
+    const expression1 = "3 4 2 + *";
+const result1 = evaluatePostfix(expression1);
+console.log(`${expression1} = ${result1}`)
+
+const expression = "3 4 + 2 * 5 -";
+const result = evaluatePostfix(expression);
+console.log(`${expression} = ${result}`)
